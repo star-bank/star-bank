1caa5a0f6695147ff1d7106f9e8c91e337ead236
Barbara Uribe <barbs@Barbaras-MacBook-Pro.local>
2020-11-10 22:57:20 -0800

    First Sql Statement

--- Statements.sql
+++ Statements.sql
@@ -1,1 +1,17 @@
 
+SELECT c_name, cs_starNames
+FROM ConstellationStars, Constellations
+WHERE c_constellationkey = cs_constellationkey;
+
+SELECT c_name, MAX(cs_amountofstars)
+FROM ConstellationStars, Constellations
+WHERE cs_constellationkey=c_constellationkey;
+
+SELECT c_name,cs_amountofstars
+FROM ConstellationStars, Constellations
+WHERE cs_constellationkey=c_constellationkey
+and cs_amountofstars = (SELECT MIN(cs_amountofstars) FROM ConstellationStars)
+ORDER BY c_name;
+
+SELECT SUM(cs_amountofstars)
+from ConstellationStars;
