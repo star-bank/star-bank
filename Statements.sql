8f09f326a5f030c0723ad558e4319a90a2c4c1c2
Barbara Uribe <barbs@Barbaras-MacBook-Pro.local>
2020-11-12 15:16:51 -0800

    FINISHED ALL Location Based Sql Statements

--- Statements.sql
+++ Statements.sql
@@ -1,6 +1,4 @@
 
-/*Bob lives in Sri Lanka */
-INSERT INTO Users Values ('#0003','Bob', '7jbdaj72', 79.861244, 6.927079,'2020-10-31 06:56:23','IST');
 /*Steve is in Seattle */
 INSERT INTO Users Values ('#0004','Steve', '7nfsk72', -122.335167, 47.608013,'2020-01-13 06:56:23','PST');
 
@@ -34,6 +32,19 @@ ORDER BY c_name;
 SELECT SUM(cs_amountofstars)
 from ConstellationStars;
 
+/* Star Clusters Facts */
+
+SELECT *
+FROM StarClusters
+WHERE sc_name= 'M3';
+
+SELECT sc_name
+FROM StarClusters
+WHERE sc_type = 'Open';
+
+SELECT *
+from Stars
+WHERE s_name = 'Macondo';
 
 /* W is Negative Longitude and E is Postive Longitude*/
 /* S is Negative Latitude and N is Postive Latitude*/
@@ -116,6 +127,99 @@ WHERE M.u_userkey = r2.u_userkey)r4
 WHERE r4.LST >= 10)T1;
 
 
+SELECT F.l_ascension
+FROM Location F, (SELECT T1.H - strftime('%H', L.l_ascension) as hD, T1.M-strftime('%M', L.l_ascension) as mD, L.l_ascension
+FROM Location L, (SELECT SUBSTR(r4.LST,1,1)as H, SUBSTR(r4.LST,2,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST < 10
+UNION
+SELECT SUBSTR(r4.LST,1,2)as H, SUBSTR(r4.LST,3,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST >= 10)T1)TF
+WHERE TF.l_ascension = F.l_ascension
+GROUP BY F.l_ascension
+HAVING TF.hd > 0 and TF.mD >0;
 
-DELETE FROM Users where u_name ='Bob';
+
+UPDATE Location
+SET l_ulongitude = +l_longitude
+WHERE l_ascension IN (SELECT F.l_ascension
+FROM Location F, (SELECT T1.H - strftime('%H', L.l_ascension) as hD, T1.M-strftime('%M', L.l_ascension) as mD, L.l_ascension
+FROM Location L, (SELECT SUBSTR(r4.LST,1,1)as H, SUBSTR(r4.LST,2,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST < 10
+UNION
+SELECT SUBSTR(r4.LST,1,2)as H, SUBSTR(r4.LST,3,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST >= 10)T1)TF
+WHERE TF.l_ascension = F.l_ascension
+GROUP BY F.l_ascension
+HAVING TF.hd >= 0 OR TF.mD >= 0);
+
+
+UPDATE Location
+SET l_ulongitude = -l_longitude
+WHERE l_ascension IN (SELECT F.l_ascension
+FROM Location F, (SELECT T1.H - strftime('%H', L.l_ascension) as hD, T1.M-strftime('%M', L.l_ascension) as mD, L.l_ascension
+FROM Location L, (SELECT SUBSTR(r4.LST,1,1)as H, SUBSTR(r4.LST,2,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST < 10
+UNION
+SELECT SUBSTR(r4.LST,1,2)as H, SUBSTR(r4.LST,3,5)*60 as M
+FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
+from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
+from Users U,(SELECT datetime(u_dateandtime,'utc') as UniTime, u_userkey
+FROM Users
+Where u_timezone = 'PST')r1
+Where U.u_userkey = r1.u_userkey)r2
+WHERE M.u_userkey = r2.u_userkey)r4
+WHERE r4.LST >= 10)T1)TF
+WHERE TF.l_ascension = F.l_ascension
+GROUP BY F.l_ascension
+HAVING TF.hd < 0 or TF.mD < 0);
+
+SELECT *
+FROM Location;
+
+SELECT DISTINCT s_name as StarsNear
+from Stars, Location, Users
+WHERE u_name='Steve' and l_latitude <= u_latitude + 75 and l_latitude >= u_latitude - 75 and 
+l_ulongitude <= u_longitude +75 and l_latitude >= u_longitude - 75 and l_locationkey=s_locationkey;
+
+
+SELECT DISTINCT c_name as ConstellationsNear
+from Stars, Location, Users, Constellations
+WHERE u_name='Steve' and l_latitude <= u_latitude + 75 and l_latitude >= u_latitude - 75 and 
+l_ulongitude <= u_longitude +75 and l_latitude >= u_longitude - 75 and l_locationkey=s_locationkey and s_constellation= c_name;
+
 DELETE FROM Users where u_name ='Steve';
