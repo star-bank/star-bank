7855b9b8cb84e689afd5cf12193103cf49a4e245
Barbara Uribe <barbs@Barbaras-MacBook-Pro.local>
2020-11-11 16:34:00 -0800

    Statements we have so far

--- Statements.sql
+++ Statements.sql
@@ -15,3 +15,19 @@ ORDER BY c_name;
 
 SELECT SUM(cs_amountofstars)
 from ConstellationStars;
+
+
+/* W is Negative Longitude and E is Postive Longitude*/
+/* S is Negative Latitude and N is Postive Latitude*/
+
+/* HOW to Calculate LST
+Given the local longitude, date and time.
+Convert the date and time to UTC.
+Calculate Greenwich mean sidereal time (GMST).
+Using the local longitude, shift GMST to LST.
+Display LST in hours, minutes, and seconds. */
+
+
+SELECT 
+From Users
+WHERE u_timezone = 'PST';
