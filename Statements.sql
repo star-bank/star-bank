29e00978ca7c5d2c31a4e041292a38152338a23e
Barbara Uribe <barbs@Barbaras-MacBook-Pro.local>
2020-11-15 12:15:29 -0800

    Add one more user to the DB /changed the statement

--- Statements.sql
+++ Statements.sql
@@ -1,4 +1,7 @@
 
+/*We have to run the whole query because it is similar to the user inputting their data 
+and then we would give them their nearby constellations if there is any */
+
 /*Steve is in Seattle */
 INSERT INTO Users Values ('#0002','Steve', '7nfsk72', -122.335167, 47.608013,'2020-01-13 06:56:23','PST');
 
@@ -38,26 +41,13 @@ SELECT *
 FROM StarClusters
 WHERE sc_name= 'M3';
 
-SELECT sc_name AS 'Open Clusters'
+SELECT sc_name
 FROM StarClusters
 WHERE sc_type = 'Open';
 
 SELECT *
 from Stars
 WHERE s_name = 'Macondo';
-
-SELECT COUNT(sc_type) AS 'Number of Globular Clusters'
-FROM StarClusters
-WHERE sc_type = 'Globular';
-
-SELECT sc_key, sc_constellation AS 'Constellation Locations of Star Clusters'
-FROM StarClusters
-GROUP BY sc_constellation;
-
-/* about star types */
-SELECT *
-FROM StarType
-WHERE st_color like 'blue%';
 
 /* W is Negative Longitude and E is Postive Longitude*/
 /* S is Negative Latitude and N is Postive Latitude*/
@@ -96,7 +86,7 @@ Where u_timezone = 'PST')r1
 Where U.u_userkey = r1.u_userkey)r2
 WHERE M.u_userkey = r2.u_userkey;
 
-
+/*
 SELECT SUBSTR(r4.LST,1,1)as H, SUBSTR(r4.LST,2,5)*60 as M
 FROM (SELECT ((280.46061837+360.98564736629*(r2.t1*36525)+0.000387933*(r2.t1*r2.t1) - (r2.t1*r2.t1*r2.t1)/38710000)%360 + u_longitude)/15 as LST
 from Users M, (SELECT U.u_userkey, (strftime ('%J', r1.UniTime) - 2451545.0)/36525 as t1
@@ -137,7 +127,7 @@ FROM Users
 Where u_timezone = 'PST')r1
 Where U.u_userkey = r1.u_userkey)r2
 WHERE M.u_userkey = r2.u_userkey)r4
-WHERE r4.LST >= 10)T1;
+WHERE r4.LST >= 10)T1; */
 
 SELECT sc_constellation AS 'Constellation Locations of Star Clusters'
 FROM StarClusters
@@ -148,7 +138,7 @@ FROM Constellations
 WHERE c_name = (SELECT sc_constellation
 FROM StarClusters
 GROUP BY sc_constellation);
-
+/*
 SELECT F.l_ascension
 FROM Location F, (SELECT T1.H - strftime('%H', L.l_ascension) as hD, T1.M-strftime('%M', L.l_ascension) as mD, L.l_ascension
 FROM Location L, (SELECT SUBSTR(r4.LST,1,1)as H, SUBSTR(r4.LST,2,5)*60 as M
@@ -173,7 +163,7 @@ WHERE r4.LST >= 10)T1)TF
 WHERE TF.l_ascension = F.l_ascension
 GROUP BY F.l_ascension
 HAVING TF.hd > 0 and TF.mD >0;
-
+*/
 
 UPDATE Location
 SET l_ulongitude = +l_longitude
@@ -233,6 +223,7 @@ HAVING TF.hd < 0 or TF.mD < 0);
 SELECT *
 FROM Location;
 
+/*This distance seems large but we have yet to add more data to our location table*/
 SELECT DISTINCT s_name as StarsNear
 from Stars, Location, Users
 WHERE u_name='Steve' and l_latitude <= u_latitude + 75 and l_latitude >= u_latitude - 75 and 
