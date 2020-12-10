14b9532d96327b0a7faabb669426a1d447be8dae
Loreta Camacho <loretacamacho@Loretas-Mini.home>
2020-12-09 20:43:52 -0800

    with login

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+     if (isset($_GET["login"])){
+          class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('star-bank1.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      //echo "Opened database successfully\n";
+   }
+
+   $sql ='SELECT * from Users where USERNAME="'.$_POST["usr_name"].'";';
+
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      $id=$row['ID'];
+      $username=$row["USERNAME"];
+      $password=$row['PASSWORD'];
+  }
+    if ($id!=""){
+        if ($password==$_POST["pwd"]){
+           $_SESSION["login"]=$username;
+           header('Location: index.php');    
+        }else{
+          
+          echo "Wrong Password";
+        }
+      }else{
+       echo "User not exist, please register to continue!";
+      }
+   //echo "Operation done successfully\n";
+   $db->close();
+     }
+
+?>
